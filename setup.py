7e2de6859c498611fa4e78b60ebf81c2d6f09d46
lsl0102 <lslong0102@gmail.com>
2026-02-27 19:38:42 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from setuptools import setup, find_packages
 import os
 
-# 读取 README.md 作为长描述
 with open(os.path.join(os.path.dirname(__file__), "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
     name="SigflowVelo",
-    version="0.1.1",  # 记得每次上传前更新版本号
+    version="0.1.1", 
     description="A Deep Learning framework for Cell-Cell Communication Velocity",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -21,8 +20,9 @@ setup(
         "matplotlib==3.7.5",
         "scipy==1.10.1"
     ],
-    author="Your Name",
-    author_email="your.email@example.com", 
-    url="https://github.com/yourusername/SigflowVelo", 
+    author="lisilong",
+    author_email="lislong@mail2.sysu.edu.cn", 
+    url="https://github.com/lsl0102/SigflowVelo", 
     python_requires='>=3.8',
+
 )
